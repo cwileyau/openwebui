dd399f438ccacb0adff40adfb36cc3b2eda216d0
cwileyau <55563827+cwileyau@users.noreply.github.com>
2026-07-18 15:47:42 +1000

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 docker run --rm -v /home/craig/openwebui/volume:/data -v $(pwd):/backup   alpine tar czf /backup/openwebui-$(date +%Y%m%d).tar.gz /data
 docker compose pull
 docker compose up -d --remove-orphans
-docker image prune
+docker image prune -a -f
